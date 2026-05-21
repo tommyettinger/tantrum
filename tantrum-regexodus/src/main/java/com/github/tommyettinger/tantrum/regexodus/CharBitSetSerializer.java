bef8635e60cdc0891969977eff58f6cb61aa46d9
Tommy Ettinger <tommy.ettinger@gmail.com>
2026-05-21 15:34:16 -0700

    tantrum-regexodus needed a little work.

--- tantrum-regexodus/src/main/java/com/github/tommyettinger/tantrum/regexodus/CharBitSetSerializer.java
+++ tantrum-regexodus/src/main/java/com/github/tommyettinger/tantrum/regexodus/CharBitSetSerializer.java
@@ -4,9 +4,6 @@ import org.apache.fory.Fory;
 import org.apache.fory.config.Config;
 import org.apache.fory.context.ReadContext;
 import org.apache.fory.context.WriteContext;
-import org.apache.fory.memory.BoundsChecking;
-import org.apache.fory.memory.MemoryBuffer;
-import org.apache.fory.memory.Platform;
 import org.apache.fory.serializer.Serializer;
 import regexodus.ds.CharBitSet;
 
@@ -21,35 +18,16 @@ public class CharBitSetSerializer extends Serializer<CharBitSet> {
     @Override
     public void write(final WriteContext fory, final CharBitSet data) {
         final int[] bits = data.getRawBits();
-        fory.getBuffer().writePrimitiveArrayWithSize(bits, Platform.INT_ARRAY_OFFSET, bits.length << 2);
+        fory.getBuffer().writeIntsWithSize(bits);
     }
 
     @Override
     public CharBitSet read(ReadContext fory) {
         final CharBitSet obs = new CharBitSet();
-        obs.setRawBits(readIntsAndSize(fory.getBuffer()));
+        final int numElements = fory.getBuffer().readBinarySize() >>> 2;
+        int[] values = new int[numElements];
+        fory.getBuffer().readInts(values, 0, numElements);
+        obs.setRawBits(values);
         return obs;
     }
-
-    /**
-     * This method should be used to read data written by
-     * {@link MemoryBuffer#writePrimitiveArrayWithSize(Object, int, int)}.
-     */
-    private static int[] readIntsAndSize(MemoryBuffer buffer) {
-        final int numBytes = buffer.readVarUint32();
-        int readerIdx = buffer.readerIndex();
-        final int size = buffer.size();
-        // use subtract to avoid overflow
-        if (BoundsChecking.BOUNDS_CHECKING_ENABLED && readerIdx > size - numBytes) {
-            throw new IndexOutOfBoundsException(
-                    String.format(
-                            "readerIdx(%d) + length(%d) exceeds size(%d): %s", readerIdx, numBytes, size, buffer));
-        }
-        final int[] ints = new int[numBytes >>> 2];
-        Platform.copyMemory(
-                buffer.getHeapMemory(), buffer.getUnsafeAddress() + readerIdx, ints, Platform.INT_ARRAY_OFFSET, numBytes);
-        buffer._increaseReaderIndexUnsafe(numBytes);
-        return ints;
-    }
-
 }
